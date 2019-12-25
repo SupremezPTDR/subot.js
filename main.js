927800999a16fb37cd66988529f103583afb5af7
unknown <kyle.calesse@gmail.com>
2019-12-25 21:48:07 +0100

    Changement du BOT

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-const { Client } = require('discord.js');
+const { Client } = require("discord.js");
+const { TOKEN, PREFIX } = require("./config");
 const client = new Client({ disableEveryone: true });
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+client.on("ready", () => {
+  console.log("Je suis prêt");
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') msg.channel.send('Pong!');
-  if (msg.content === 'everyone') msg.channel.send('@everyone, Salut à tous !', { disableEveryone: false });  
-  if (msg.content === 'noteveryone') msg.channel.send('@everyone (noteveryone), Salut à tous');  
+client.on("message", msg => {
+  if (msg.content.startsWith(`${PREFIX}ping`)) msg.channel.send("Pong!");
+  if (msg.content.startsWith(`${PREFIX}pong`)) msg.channel.send("Pong!");
 });
 
-client.login('NjQxNjczMzc5NzkwNzE2OTY5.XgO7dQ.SmKU9C-17fpnBUDbVgQOD7HwvHg'); 
+client.login(TOKEN);
